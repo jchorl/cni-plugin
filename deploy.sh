ae748dffc3dc7d15ed3b1a426f31e30ea282c028
Josh Chorlton <jchorlton@gmail.com>
2020-04-18 11:22:24 -0700

    push to latest-arm

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,4 @@ echo "$2" | docker login -u "$1" --password-stdin
 
 docker images
 
-docker tag jchorl/cni:latest-arm jchorl/cni:latest
-docker push jchorl/cni:latest
+docker push jchorl/cni:latest-arm
